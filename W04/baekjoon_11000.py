23e02fec06a65b8678a19bb002af6d7d1f2a487e
jbpark0919 <66005132+jbpark0919@users.noreply.github.com>
2024-08-25 18:44:37 +0900

    Create baekjoon_11000.py

---
+++ W04/baekjoon_11000.py
@@ -0,0 +1,21 @@
+import heapq
+
+n = int(input())
+
+times = []
+
+for i in range(n):
+    s, e = map(int, input().split())
+    times.append([s, e])
+
+times.sort()
+
+room = []
+heapq.heappush(room, times[0][1])
+
+for i in range(1, n):
+    if times[i][0] >= room[0]:
+        heapq.heappop(room)
+    heapq.heappush(room, times[i][1])
+
+print(len(room))
